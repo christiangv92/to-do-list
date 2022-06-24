4f673ccb567a375b164768d4a021d8726e6dd6cd
Christian Veiga <christian.veiga92@gmail.com>
2022-06-24 14:38:48 -0300

    footer and new style

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
-
     const html = ` 
         <li class="list-group-item d-flex justify-content-between align-items-center">
             <span>${todo}</span>
             <i class="fa-solid fa-trash delete"></i>
         </li> 
     `;
-
     list.innerHTML += html;
-
 };
 
 addForm.addEventListener('submit', e => {
-
     e.preventDefault();
     const todo = addForm.add.value.trim();
     console.log(todo);
@@ -24,14 +21,28 @@ addForm.addEventListener('submit', e => {
      generateTemplate(todo);
      addForm.reset();
     }
-
 });
 
 //delete to-dos
-list.addEventListener('click', e => {
-    
+list.addEventListener('click', e => {    
     if(e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
     }
+});
 
+const filterTodos = (term) => {
+
+    Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+//keyup event
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
